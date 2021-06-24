f0615c8304cf31ac8ca822196c0e396a54aea000
AlexandrShcherbackov <jsprog2017@yandex.ru>
2021-06-24 16:07:56 +0300

    implemented a custom input and a form with calculations

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,69 @@
 <template>
-  <div id="app">
-    test
+  <div id="app" class="flex items-center justify-center h-screen">
+    <div class="flex flex-col p-8">
+      <div class="flex flex-row items-center space-x-4">
+        <img src="@/assets/folder.svg" alt="" class="w-6 h-6">
+        <ul class="flex flex-col items-start ">
+          <li>{{kick}} <span class="text-limegreen">KICK</span></li>
+          <li>{{eth}} <span class="text-indianred">ETH</span></li>
+        </ul>
+      </div>
+      <div class="flex flex-col w-440px space-y-4 mt-4">
+        <KickEcoInput
+          v-model="price"
+          prependText="Price"
+          appendText="ETH"
+        />
+        <KickEcoInput
+          v-model="amount"
+          prependText="Amount"
+          appendText="KICK"
+        />
+        <KickEcoInput
+          v-model="total"
+          prependText="Total ~"
+          appendText="ETH"
+          placeholder="min 0.005"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import KickEcoInput from '@/components/KickEcoInput.vue';
+
 export default {
   name: 'App',
+  components: {
+    KickEcoInput,
+  },
+  data() {
+    return {
+      price: '',
+      amount: '',
+      total: '',
+      kick: 0,
+      eth: 0,
+    };
+  },
+  watch: {
+    price(v) {
+      if (this.amount !== '') {
+        this.total = Number(v) * Number(this.amount);
+      }
+    },
+    amount(v) {
+      if (this.price !== '') {
+        this.total = Number(v) * Number(this.price);
+      }
+    },
+    total(v) {
+      if (this.price !== '') {
+        this.amount = Number(v) / Number(this.price);
+      }
+    },
+  },
 };
 </script>
 
---
+++ src/components/KickEcoInput.vue
@@ -0,0 +1,129 @@
+<template>
+  <FormInput
+    v-model="currentValue"
+    :placeholder="placeholder"
+    :focusRef="focusRef"
+    classes="rounded-lg overflow-hidden "
+    inputClass="kickeco-input text-right text-base"
+    @blur="blurHandler"
+    @keyup.up="increment"
+    @keyup.down="decrement"
+    @keypress.enter="$emit('enter')"
+  >
+    <template #prepend>
+      <div class="flex items-center">
+        <span class="text-gray-400 whitespace-nowrap select-none">{{prependText}}</span>
+      </div>
+    </template>
+    <template #append>
+      <div class="flex flex-row space-x-2">
+        <div class="flex items-center">
+          <span class="text-gray-400 select-none">{{appendText}}</span>
+        </div>
+        <div class="flex flex-col">
+          <button
+            :class="[
+              'flex items-center justify-center',
+              'w-8 h-1/2',
+              'focus:outline-none border-l border-b border-whitesmoke bg-snow',
+            ]"
+            @click="increment"
+          >
+            <span class="vertical-arrow active"></span>
+          </button>
+          <button
+            :class="[
+              'flex items-center justify-center',
+              'w-8 h-1/2',
+              'focus:outline-none border-l border-whitesmoke bg-snow',
+              { 'disabled': !Number(currentValue) },
+            ]"
+            :disabled="!Number(currentValue)"
+            @click="decrement"
+          >
+            <span class="vertical-arrow"></span>
+          </button>
+        </div>
+      </div>
+    </template>
+  </FormInput>
+</template>
+<script>
+import FormInput from '@/components/FormInput.vue';
+
+export default {
+  name: 'KickEcoInput',
+  components: {
+    FormInput,
+  },
+  props: {
+    value: {
+      type: [String, Number],
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    prependText: {
+      type: String,
+      default: '',
+    },
+    appendText: {
+      type: String,
+      default: '',
+    },
+    focusRef: {
+      type: String,
+      default: 'input',
+    },
+  },
+  data() {
+    return {
+      currentValue: null,
+    };
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler(v) {
+        const [q1, ...q2] = String(v)
+          .replace(/[^0-9,.]/g, '')
+          .replace(/,/ig, '.')
+          .replace(/(\d+\.\d{1,8})/ig, '$1')
+          .split('.');
+        this.currentValue = `${q1}${q2.length ? `.${q2.join('').slice(0, 8)}` : ''}`;
+      },
+    },
+    currentValue(v) {
+      this.$emit('input', v);
+    },
+  },
+  methods: {
+    blurHandler() {
+      const lastChar = this.currentValue[this.currentValue.length - 1];
+
+      if (lastChar === '.') {
+        this.currentValue = this.currentValue.slice(0, this.currentValue.length - 1);
+      }
+    },
+    increment() {
+      this.currentValue = this.currentValue.replace(/\d+$/, (m) => parseInt(m, 10) + 1);
+    },
+    decrement() {
+      this.currentValue = this.currentValue.replace(/\d+$/, (m) => parseInt(m, 10) - 1);
+    },
+  },
+};
+</script>
+<style lang="scss">
+.kickeco-input {
+  &::placeholder {
+    @apply text-right;
+  }
+}
+
+button.disabled {
+  opacity: 0.4;
+}
+</style>
