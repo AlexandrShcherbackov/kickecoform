67b44e8eff953c3163279c338a666643bbc139d1
AlexandrShcherbackov <jsprog2017@yandex.ru>
2021-06-28 11:47:44 +0300

    fixed calculate bug

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,23 @@
       </div>
       <div class="flex flex-col w-440px space-y-4 mt-4">
         <KickEcoInput
-          v-model="price"
+          :value="price"
           prependText="Price"
           appendText="ETH"
+          @input="handlePrice"
         />
         <KickEcoInput
-          v-model="amount"
+          :value="amount"
           prependText="Amount"
           appendText="KICK"
+          @input="handleAmount"
         />
         <KickEcoInput
-          v-model="total"
+          :value="total"
           prependText="Total ~"
           appendText="ETH"
           placeholder="min 0.005"
+          @input="handleTotal"
         />
       </div>
       <div class="flex flex-row items-center justify-between space-x-4 mt-12">
@@ -67,24 +70,37 @@ export default {
       eth: 0,
     };
   },
-  watch: {
-    price(v) {
-      if (this.amount !== '') {
-        this.total = Number(v) * Number(this.amount);
-      }
-    },
-    amount(v) {
-      if (this.price !== '') {
-        this.total = Number(v) * Number(this.price);
-      }
-    },
-    total(v) {
-      if (this.price !== '') {
-        this.amount = Number(v) / Number(this.price);
-      }
-    },
-  },
   methods: {
+    handlePrice(v) {
+      this.price = v;
+      this.calcTotal();
+    },
+    handleAmount(v) {
+      this.amount = v;
+      this.calcTotal();
+    },
+    handleTotal(v) {
+      this.total = v;
+
+      if (this.total === '' || this.price === '') {
+        this.amount = '';
+      }
+
+      const decimalPart = String(this.amount).match(/\.\d+$/);
+      const decimalLength = decimalPart ? decimalPart[0].length - 1 : 0;
+      const newAmount = parseFloat(this.total) / parseFloat(this.price);
+      this.amount = decimalLength > 0 ? (newAmount).toFixed(decimalLength) : newAmount;
+    },
+    calcTotal() {
+      if (this.amount === '' || this.price === '') {
+        this.total = '';
+        return;
+      }
+
+      const price = this.price.replace(/\.$/, '');
+      const amount = this.amount.replace(/\.$/, '');
+      this.total = parseFloat(price) * parseFloat(amount);
+    },
     buyCrypto() {
       this.kick += Number(this.amount);
       this.eth += Number(this.total);
